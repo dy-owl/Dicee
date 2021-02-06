12323b2891fb7ae6b5c8f3f314596b76450e23cc
Dmytro Yantsybaiev <d.yantsybaiev@icloud.com>
2021-02-06 21:32:48 +0200

    The Dices now randomly change from 1 to 6 when the button is pressed.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,15 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
+    var leftDiceNumber = Int.random(in: 0...5)
+    var rightDiceNumber = Int.random(in: 0...5)
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
         diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
         
-        leftDiceNumber += 1
-        rightDiceNumber -= 1
+        leftDiceNumber = Int.random(in: 0...5)
+        rightDiceNumber = Int.random(in: 0...5)
     }
     
 }
